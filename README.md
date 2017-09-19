c2bfac8496a201f8e633ee94e85df863446daab2
안석준 <sjahn@ex-em.com>
2017-09-19 15:47:06 +0900

    read blog article list done

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # flask-example
 Flask example application
+
+## Resolving dependencies
+
+```shell
+pip install flask-mysql
+```
+
+## How to initialize database
+type `​mysql -u root -p` and type again
+
+```sql
+CREATE DATABASE blog;
+
+USE blog;
+
+CREATE TABLE IF NOT EXISTS `blog` (
+`articleNumber` int(10) unsigned NOT NULL auto_increment COMMENT '글번호',
+`title` mediumtext collate utf8_unicode_ci NOT NULL COMMENT '제목',
+`content` varchar(1000) collate utf8_unicode_ci NOT NULL COMMENT '본문',
+UNIQUE KEY `articleNumber` (`articleNumber`)
+);
+
+show tables;
+
+insert into blog (title, content) values("title", "body");
+``` 
--- app.py
+++ app.py
@@ -1,10 +1,51 @@
 # hello.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'blog'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 
 @app.route("/hello/")
 def hello(name=None):
 	return render_template('hello.html', name=name)
+
+@app.route("/loadData")
+def loadData():
+	cursor = mysql.connect().cursor()
+	cursor.execute("select * from blog")
+
+	result = []
+
+	for row in cursor:
+		result.append(row)
+
+	print (result)
+	return "load Test" + ''.join(str(e) for e in result)
+
+
+@app.route("/home")
+def home():
+	cursor = mysql.connect().cursor()
+	cursor.execute("select * from blog")
+
+	posts = []
+
+	for row in cursor:
+		post = {}
+		post['href'] = "./{}".format(row[0])
+		post["title"] = row[1]
+		post["content"] = row[2]
+		posts.append(post)
+
+
+	return render_template('home.html', posts=posts)
 	
 if __name__ == "__main__":
 	app.run()
